38da0c95ab36eb593af35809f073104c3b1ff937
何禾 <Hoho@hehedeMacBook-Pro.local>
2016-10-02 03:27:47 +0800

    商品detail頁面顯示

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -2,3 +2,11 @@ $("#item_img_indicators li").on("click", function(){
    $("#item_img_indicators li").parent().find(".active").removeClass("active");
    $(this).addClass("active");
 });
+
+// $(function () {
+//   $('[data-toggle="tooltip"]').tooltip({trigger: 'manual'}).tooltip('show');
+// });
+
+$(function () {
+	$('a[rel=popover]').popover({trigger: 'manual'}).popover('show');
+});
